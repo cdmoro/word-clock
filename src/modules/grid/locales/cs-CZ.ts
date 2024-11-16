a3364c694477e2627a35f87bf78b4f97d5204b4f
Carlos Bonadeo <carlosbonadeo@gmail.com>
2024-11-16 22:49:53 +0000

    Czech 99%

--- src/modules/grid/locales/cs-CZ.ts
+++ src/modules/grid/locales/cs-CZ.ts
@@ -19,38 +19,49 @@ const commonWords: CommonWords = {
   TWO: [19, 20, 21], // DVĚ
   THREE: [37, 38, 39], // TŘI
   FOUR: [55, 56, 57, 58, 59], // ČTYŘI
-  FIVE: [107, 108, 109], // PĚT
-  SIX: [41, 42, 43, 44], // ŠEST
+  FIVE: [16, 17, 18], // PĚT
+  SIX: [40, 41, 42, 43], // ŠEST
   SEVEN: [22, 23, 24, 25], // SEDM
   EIGHT: [44, 45, 46], // OSM
   NINE: [11, 12, 13, 14, 15], // DEVĚT
   TEN: [61, 62, 63, 64, 54], // DESET
   ELEVEN: [47, 48, 49, 50, 51, 52, 53, 54], // JEDENÁCT
-  FIVE_MIN: [16, 17, 18], // PĚT (cinco minutos)
-  TEN_MIN: [33, 34, 35, 36, 37], // DESET (diez minutos)
-  QUARTER_MIN: [77, 78, 79, 80, 81, 82, 83], // PATNÁCT (quince minutos)
-  TWENTY_MIN: [66, 67, 68, 69, 70, 71], // DVACET (veinte minutos)
-  TWENTYFIVE_MIN: [88, 89, 90, 91, 92, 93, 94],
+  FIVE_MIN: (_hours, minutes) =>
+    minutes < 30
+      ? [107, 108, 109]
+      : [
+          [99, 100, 101, 102, 103, 104, 105],
+          [107, 108, 109],
+        ], // PĚT
+  TEN_MIN: (_hours, minutes) => (minutes < 30 ? [33, 34, 35, 36, 37] : [99, 100, 101, 102, 103, 104, 105]), // DESET (diez minutos)
+  QUARTER_MIN: (_hours, minutes) =>
+    minutes < 30
+      ? [77, 78, 79, 80, 81, 82, 83]
+      : [
+          [90, 91, 92, 93, 94, 95, 96, 97],
+          [107, 108, 109],
+        ], // PATNÁCT
+  TWENTY_MIN: (_hours, minutes) => (minutes < 30 ? [66, 67, 68, 69, 70, 71] : [90, 91, 92, 93, 94, 95, 96, 97]), // DVACET (veinte minutos)
+  TWENTYFIVE_MIN: (_hours, minutes) =>
+    minutes < 30
+      ? [
+          [66, 67, 68, 69, 70, 71],
+          [107, 108, 109],
+        ]
+      : [
+          [71, 72, 73, 74, 75, 76],
+          [107, 108, 109],
+        ],
   HALF: [71, 72, 73, 74, 75, 76], // TŘICET
 };
 
 const localeWords = {
   JE: [0, 1], // JE
-  JSOU: [2, 3, 4], // JSOU
-  //   OVER: [88, 89, 90, 91, 92], // ČTYŘICET
+  JSOU: [2, 3, 4, 5], // JSOU
 };
 
-function getLocaleWordKeys(_hours: number, minutes: number) {
-  const wordKeys: WordKeys<typeof localeWords>[] = minutes === 0 ? ['JE'] : ['JSOU'];
-
-  // Lógica para determinar "HALF" o "OVER"
-  if (minutes >= 25 && minutes <= 35) {
-    wordKeys.push('HALF');
-  }
-  //   else if (minutes > 35) {
-  //     wordKeys.push('OVER');
-  //   }
-
+function getLocaleWordKeys(_hours: number, _minutes: number) {
+  const wordKeys: WordKeys<typeof localeWords>[] = ['JE'];
   return wordKeys;
 }
 
