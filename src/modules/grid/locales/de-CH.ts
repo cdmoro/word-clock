42546b11cd249165beb18ff7a3984eae0dc28e63
Carlos Bonadeo <carlosbonadeo@gmail.com>
2024-11-26 22:06:25 +0000

    Swiss German 50%

--- src/modules/grid/locales/de-CH.ts
+++ src/modules/grid/locales/de-CH.ts
@@ -27,21 +27,21 @@ const commonWords: CommonWords = {
   ELEVEN: [92, 93, 94], // ELF
   TWELVE: [99, 100, 101, 102, 103, 104], // ZWÖUFI
   FIVE_MIN: [8, 9, 10], // FÜF
-  TEN_MIN: [11, 12, 13], // ZÄÄ
-  QUARTER_MIN: [15, 16, 17, 18, 19], // VIERTU
-  TWENTY_MIN: [22, 23, 24, 25, 26], // ZWÄNZG
+  TEN_MIN: [19, 20, 21], // ZÄÄ
+  QUARTER_MIN: [11, 12, 13, 14, 15, 16], // VIERTU
+  TWENTY_MIN: [22, 23, 24, 25, 26, 27], // ZWÄNZG
   TWENTYFIVE_MIN: [
-    // [0, 1, 2, 3], // FÜF
+    [8, 9, 10], // FÜF
     [22, 23, 24, 25, 26], // ZWÄNZG
   ],
-  HALF: [36, 37, 38, 39], // HAUBI
+  HALF: [36, 37, 38, 39, 40], // HAUBI
 };
 
 const localeWords = {
   ES: [0, 1], // ES
   ISCH: [3, 4, 5, 6], // ISCH
-  VOR: [27, 28, 29], // VOR
-  AB: [22, 23], // AB (AFTER)
+  VOR: [30, 31, 32], // VOR
+  AB: [33, 34], // AB (AFTER)
   HAUBI: [33, 34, 35, 36], // HAUBI (HALF)
   UHR: [107, 108, 109], // UHR (O'CLOCK)
 };
@@ -77,23 +77,23 @@ export default {
   },
   examples: {
     '01:00': 'ES ISCH EIS UHR', // Exact hour
-    // '01:05': 'ES ISCH FÜF AB EIS', // 5 past 1
-    // '01:10': 'ES ISCH ZÄÄ AB EIS', // 10 past 1
-    // '01:15': 'ES ISCH VIERTU AB EIS', // Quarter past 1
-    // '01:20': 'ES ISCH ZWÄNZG AB EIS', // 20 past 1
-    // '01:25': 'ES ISCH FÜF VOR HAUBI ZWÖI', // 25 past 1
-    // '01:30': 'ES ISCH HAUBI ZWÖI', // Half-past 1
-    // '01:35': 'ES ISCH FÜF AB HAUBI ZWÖI', // 5 past half 2
-    // '01:40': 'ES ISCH ZWÄNZG VOR ZWÖI', // 20 to 2
-    // '01:45': 'ES ISCH VIERTU VOR ZWÖI', // Quarter to 2
-    // '01:50': 'ES ISCH ZÄÄ VOR ZWÖI', // 10 to 2
-    // '01:55': 'ES ISCH FÜF VOR ZWÖI', // 5 to 2
-    // '02:00': 'ES ISCH ZWÖI UHR', // Exact hour
-    // '07:30': 'ES ISCH HAUBI ACHTI', // Half-past 7
-    // '10:00': 'ES ISCH ZÄNI UHR', // Exact hour
-    // '12:00': 'ES ISCH ZWÖUFI UHR', // Noon
-    // '12:30': 'ES ISCH HAUBI EIS', // Half-past 12
-    // '17:05': 'ES ISCH FÜF AB FÜFI', // 5 past 5
-    // '20:26': 'ES ISCH FÜF VOR HAUBI ACHTI', // 5 before half-past 8
+    '01:05': 'ES ISCH FÜF AB EIS', // 5 past 1
+    '01:10': 'ES ISCH ZÄÄ AB EIS', // 10 past 1
+    '01:15': 'ES ISCH VIERTU AB EIS', // Quarter past 1
+    '01:20': 'ES ISCH ZWÄNZG AB EIS', // 20 past 1
+    '01:25': 'ES ISCH FÜF ZWÄNZ AB EIS', // 25 past 1
+    '01:30': 'ES ISCH HAUBI EIS', // Half-past 1
+    '01:35': 'ES ISCH FÜF ZWÄNZ VOR ZWÖI', // 5 past half 2
+    '01:40': 'ES ISCH ZWÄNZG VOR ZWÖI', // 20 to 2
+    '01:45': 'ES ISCH VIERTU VOR ZWÖI', // Quarter to 2
+    '01:50': 'ES ISCH ZÄÄ VOR ZWÖI', // 10 to 2
+    '01:55': 'ES ISCH FÜF VOR ZWÖI', // 5 to 2
+    '02:00': 'ES ISCH ZWÖI UHR', // Exact hour
+    '07:30': 'ES ISCH HAUBI SIBNI', // Half-past 7
+    '10:00': 'ES ISCH ZÄNI UHR', // Exact hour
+    '12:00': 'ES ISCH ZWÖUFI UHR', // Noon
+    '12:30': 'ES ISCH HAUBI ZWÖUFI', // Half-past 12
+    '17:05': 'ES ISCH FÜF AB FÜF', // 5 past 5
+    '20:26': 'ES ISCH FÜF ZWÄNZ AB ACHTI', // 5 before half-past 8
   },
 } satisfies LocaleGridConfig;
